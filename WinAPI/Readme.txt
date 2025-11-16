953ee8822ebb93e94b126986323dad9d23a9dfef
Danila <Danila@DESKTOP-44LP0PV>
2025-11-16 18:10:24 +0300

    Minimal symbols

---
+++ WinAPI/Readme.txt
@@ -0,0 +1,8 @@
+GetWindowLongPtr - https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowlongptra
+SetWindowLongPtr - https://learn.microsoft.com/ru-ru/windows/win32/api/winuser/nf-winuser-setwindowlongptrw
+GWLP_USERDATA - https://learn.microsoft.com/ru-ru/windows/win32/learnwin32/managing-application-state-
+DefWindowProc - https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-defwindowproca
+WM_SETFOCUS - https://learn.microsoft.com/en-us/windows/win32/inputdev/wm-setfocus
+
+GWLP_WNDPROC - Адресс оконной процедуры
+GWLP_USERDATA - Пользовательские данные
--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -10,7 +10,6 @@ struct EditData
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wPAram, LPARAM lParam);
 void AddPlaceholder(HWND hEdit, const char* placeholderText);
-LRESULT CALLBACK Placeholder(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -29,13 +28,13 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:	//Выполняется 1 раз, при запуске окна.
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
-		//SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETFOCUS, (WPARAM)GetDlgItem(hwnd,IDOK), NULL);
 		//SetFocus(GetDlgItem(hwnd, IDC_EDIT_LOGIN));
 		AddPlaceholder(GetDlgItem(hwnd, IDC_EDIT_LOGIN), "Введите логин");
 		AddPlaceholder(GetDlgItem(hwnd, IDC_EDIT_PASSWORD), "Введите пароль");
@@ -74,9 +73,28 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CLOSE:		//Выполняется при нажатии кнопки 'x'.
 		EndDialog(hwnd, 0);
 	}
-	return FALSE;
+	//Изменения в коде----------------------------------------------------------------------------------------------------------------
+	EditData* data = (EditData*)GetWindowLongPtr(hwnd, GWLP_USERDATA);	//Получил данные из USERDATA
+	if (!data) return DefWindowProc(hwnd, uMsg, wParam, lParam);		//Если данных нет - вызывается стандартная обработка
+	switch (uMsg)	//Обрабатываю сообщения windows
+	{
+	case WM_SETFOCUS:	//Окно получает фокус ввода
+		if (data->isShowingPlaceholder)
+		{
+			data->isShowingPlaceholder = false;	//Убрал подсказку
+			SetWindowTextA(hwnd, "");	//Очистил текст
+		}
+		break;
+	case WM_KILLFOCUS:	//Окно теряет фокус ввода
+		if (GetWindowTextLength(hwnd) == 0 && !data->isShowingPlaceholder)	//Если оно пустое и подсказка не отображается
+		{
+			data->isShowingPlaceholder = true;	//Показываю подсказку опять
+			SetWindowTextA(hwnd, data->placeholderText);	//Устанавливаю текст подсказки
+		}
+		break;
+	}
+	return CallWindowProc(data->originalProcedure, hwnd, uMsg, wParam, lParam); //Для всех остальных сообщений вызываю оригинальную процедуру
 }
-
 void AddPlaceholder(HWND hEdit, const char* placeholderText)
 {
 	EditData* data = new EditData;				//Создал новую структуру данных для хранения состояния placeholder'a
@@ -84,39 +102,8 @@ void AddPlaceholder(HWND hEdit, const char* placeholderText)
 	data->isShowingPlaceholder = true;			//Показал наличие этого текста
 	data->originalProcedure = (WNDPROC)GetWindowLongPtr(hEdit, GWLP_WNDPROC);//сохранил оригинал Edit control'a	SetWindowLongPtr(hEdit, GWLP_USERDATA, (LONG_PTR)data);//
 	SetWindowLongPtr(hEdit, GWLP_USERDATA, (LONG_PTR)data);//Сохранил указатель на структуру в USERDATA окна
-	SetWindowLongPtr(hEdit, GWLP_WNDPROC, (LONG_PTR)Placeholder);//Заменил оконную процедуру на новую
+	SetWindowLongPtr(hEdit, GWLP_WNDPROC, (LONG_PTR)DlgProc);//Заменил оконную процедуру на новую
 	SetWindowText(hEdit, placeholderText);		//Установил текст подсказки в поле ввода
-}
-LRESULT CALLBACK Placeholder(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	EditData* data = (EditData*)GetWindowLongPtr(hWnd, GWLP_USERDATA);	//Получил данные из USERDATA
-	if (!data) return DefWindowProc(hWnd, uMsg, wParam, lParam);		//Если данных нет - вызывается стандартная обработка
-	switch (uMsg)	//Обрабатываю сообщения windows
-	{
-	case WM_SETFOCUS:	//Окно получает фокус ввода
-		if (data->isShowingPlaceholder)
-		{
-			data->isShowingPlaceholder = false;	//Убрал подсказку
-			SetWindowTextA(hWnd, "");	//Очистил текст
-		}
-		break;
-	case WM_KILLFOCUS:	//Окно теряет фокус ввода
-		if (GetWindowTextLength(hWnd) == 0 && !data->isShowingPlaceholder)	//Если оно пустое и подсказка не отображается
-		{
-			data->isShowingPlaceholder = true;	//Показываю подсказку опять
-			SetWindowTextA(hWnd, data->placeholderText);	//Устанавливаю текст подсказки
-		}
-		break;
-	//case WM_CHAR:	//Если пользователь вводит символ
-	//	if (data->isShowingPlaceholder)
-	//	{
-	//		data->isShowingPlaceholder = false;	//Убираю подсказку при вводе
-	//		SetWindowTextA(hWnd, "");
-	//		return (wParam == VK_BACK) ? 0 : CallWindowProc(data->originalProcedure, hWnd, uMsg, wParam, lParam);//Если не Backspace - передаю символ дальше
-	//	}
-	//	break;
-	}
-	return CallWindowProc(data->originalProcedure, hWnd, uMsg, wParam, lParam); //Для всех остальных сообщений вызываю оригинальную процедуру
 }
  
  
