5275da55d743d86b9f900de85d89e76e31b2d919
Danila <Danila@DESKTOP-44LP0PV>
2025-11-19 19:43:56 +0300

    ComboBox DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -1,4 +1,6 @@
-﻿#include<Windows.h>
+﻿//#define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
+#include<cstdio>
 #include"resource.h"
 
 CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "Combo", "Box" };
@@ -22,7 +24,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
 		}
-		SendMessage(GetDlgItem(hwnd, IDC_COMBO1), CB_SETCURSEL, 0, 0);
+		//SendMessage(GetDlgItem(hwnd, IDC_COMBO1), CB_SETCURSEL, 0, 0);
 		//https://learn.microsoft.com/en-us/windows/win32/controls/cb-setcursel
 	}
 	break;
@@ -32,15 +34,30 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 		case IDOK:
 		{
-			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);	//Находим список
-			int index = (int)SendMessage(hCombo, CB_GETCURSEL, 0, 0); //Получаем индекс выбранного элемента в combobox
-			//https://learn.microsoft.com/ru-ru/windows/win32/controls/cb-getcursel
-			char selectedText[100];//Создаём буфер для хранения текста выбранного элемента
-			GetWindowTextA(hCombo, selectedText, sizeof(selectedText));//Получаем текст выбранного элемента
-			char message[200];//Создаём буфер для сообщения
-			wsprintf(message, "Вы выбрали элемент № %d со значением '%s'", index, selectedText);//Форматируем сообщение
-			//https://cplusplus.com/forum/windows/92842
-			MessageBox(hwnd, message, "Информация о выборе", MB_OK | MB_ICONINFORMATION);//Выводим его
+			//HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);	//Находим список
+			//int index = (int)SendMessage(hCombo, CB_GETCURSEL, 0, 0); //Получаем индекс выбранного элемента в combobox
+			////https://learn.microsoft.com/ru-ru/windows/win32/controls/cb-getcursel
+			//char selectedText[100];//Создаём буфер для хранения текста выбранного элемента
+			//GetWindowTextA(hCombo, selectedText, sizeof(selectedText));//Получаем текст выбранного элемента
+			//char message[200];//Создаём буфер для сообщения
+			//wsprintf(message, "Вы выбрали элемент № %d со значением '%s'", index, selectedText);//Форматируем сообщение
+			////https://cplusplus.com/forum/windows/92842
+			//MessageBox(hwnd, message, "Информация о выборе", MB_OK | MB_ICONINFORMATION);//Выводим его
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{}; //sz - String Zero(NULL Terminated Line)
+			CHAR sz_message[SIZE]{}; //sz - String Zero(NULL Terminated Line)
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0); //GetCurrentSelection - получить текущий выбранный элемент
+			if (i != CB_ERR)
+			{
+				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+				wsprintf(sz_message, "Вы выбрали элемент №%i со значением \"%s\"", i, sz_buffer);
+				MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+			}
+			else
+			{
+				MessageBox(hwnd, "Вы ничего не выбрали", "Info", MB_OK | MB_ICONINFORMATION);
+			}
 		}
 			break;
 		case IDCANCEL:
