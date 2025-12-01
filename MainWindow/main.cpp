151573215c2a93e9d86460112e7514fc3bb3634b
Danila <Danila@DESKTOP-44LP0PV>
2025-12-01 18:33:07 +0300

    opisanie DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -5,6 +5,7 @@
 CONST CHAR g_sz_WINDOW_CLASS[] = "My First Window";
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void WindowTitle(HWND hwnd);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -46,15 +47,23 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		return 0;
 	}
 
+	int displayWidth = GetSystemMetrics(SM_CXSCREEN);	//SM_CXSCREEN — возвращает ширину основного монитора в пикселях
+	int displayHeight = GetSystemMetrics(SM_CYSCREEN);	//SM_CYSCREEN — возвращает высоту основного монитора в пикселях
+
+	int windowWidth = displayWidth * 3 / 4;
+	int windowHeight = displayHeight * 3 / 4;
+
+	int windowX = (displayWidth - windowWidth) / 2;
+	int windowY = (displayHeight - windowHeight) / 2;
+
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,
 		g_sz_WINDOW_CLASS,	//Имя класса окна
 		g_sz_WINDOW_CLASS,	//Заголовок окна
 		WS_OVERLAPPEDWINDOW,//Стиль окна, ситил зависят от класса окна
-		CW_USEDEFAULT, CW_USEDEFAULT, //Position
-		CW_USEDEFAULT, CW_USEDEFAULT, //Размер окна
-		/*640, 480,*/
+		windowX, windowY, //Position
+		windowWidth, windowHeight, //Размер окна
 		NULL,
 		NULL, //Для главного окна - это ResourceID главного меню, 
 		//для дочернего окна(Control) - ResourceID дочернего окна(IDC_BUTTON_COPY)
@@ -66,6 +75,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		MessageBox(NULL, "Windows creation failed", NULL, MB_OK | MB_ICONERROR);
 		return 0;
 	}
+	WindowTitle(hwnd);
 	ShowWindow(hwnd, nCmdShow);	//Задаёт режим отображения онка - развёрнуто на весь экран, свёрнуто в окно
 	//Свёрнуто на панель задач
 	UpdateWindow(hwnd);	//Обновляет рабочую область указанного окна, отправляя сообщение 'WM_PAINT', если клиентская область окна не пустая
@@ -80,6 +90,42 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	return 0;
 }
 
+void WindowTitle(HWND hwnd)
+{
+	RECT rect;	//Структура RECT - хранит координаты окна, содержит 4 поля - left, right, top, bottom
+	GetWindowRect(hwnd, &rect); //Получаем координаты и размер окна на экране,
+	//Функция GetWindowRect заполняет структуру rect значениями
+
+	int x = rect.left;	//Извлекаем X - координату левого верхнего угла окна
+	int y = rect.top;	//Извлекаем Y-координату левого верхнего угла окна
+	int width = rect.right - rect.left;	//Вычисляем ширину окна
+	int height = rect.bottom - rect.top;//Вычисляем высоту окна
+
+	CHAR title[512];	//Создаём буфер для строки заголовка
+	CHAR temp[64];		//Создаём буфер для конкретных чисел
+
+	lstrcpy(title, "Window: Position("); //Начинаем формировать заголовок
+	//https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-lstrcpya
+	wsprintf(temp, "%d", x);			//Преобразуем координату X в строку и сохраняем в temp
+	lstrcat(title, temp);				//Добавляем координату X к заголовку
+	lstrcat(title, ", ");				//Добавляем разделитель между координатами
+	//https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-lstrcata
+	wsprintf(temp, "%d", y);			//Преобразуем координату Y в строку и делаем всё тоже самое дальше
+	lstrcat(title, temp);
+	lstrcat(title, ") Size(");
+
+	wsprintf(temp, "%d", width);		//Преобразуем ширину окна в строку
+	lstrcat(title, temp);				//Добавляем ширину к заголовку
+	lstrcat(title, " x ");				//Добавляем разделитель " x " между шириной и высотой
+
+	wsprintf(temp, "%d", height);		//Преобразуем высоту окна в строку
+	lstrcat(title, temp);				//Добавляем высоту к заголовку
+	lstrcat(title, ")");				//Закрываем заголовок
+
+	SetWindowText(hwnd, title);			//Устанавливаем новый заголовок для окна
+}
+
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
@@ -87,6 +133,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE:
 		break;
 	case WM_COMMAND:
+		break;
+	case WM_MOVE:
+		//https://learn.microsoft.com/en-us/windows/win32/winmsg/wm-move
+		WindowTitle(hwnd);
+		break;
+	case WM_SIZE:
+		WindowTitle(hwnd);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
