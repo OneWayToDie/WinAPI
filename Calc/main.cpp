094cea4321bdc5470310697b6952a373e5b01dc4
Danila <Danila@DESKTOP-44LP0PV>
2025-12-10 21:46:49 +0300

    HW_DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -393,6 +393,32 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 	}
 	break;
+	case WM_CONTEXTMENU:
+	{
+		HMENU cmMain = CreatePopupMenu();
+		AppendMenu(cmMain, MF_STRING, IDM_SQUARE_BLUE, "Square blue");
+		AppendMenu(cmMain, MF_STRING, IDM_METAL_MISTRAL, "Metal mistral");
+		AppendMenu(cmMain, MF_SEPARATOR, NULL, NULL);
+		AppendMenu(cmMain, MF_STRING, IDM_EXIT, "Exit");
+
+		BOOL selected_item = TrackPopupMenuEx
+		(
+			cmMain, 
+			TPM_RIGHTALIGN | TPM_BOTTOMALIGN | TPM_RETURNCMD | TPM_RIGHTBUTTON | TPM_VERNEGANIMATION,
+			LOWORD(lParam), HIWORD(lParam),
+			//0,	//Reserved
+			hwnd,
+			NULL
+		);
+		switch (selected_item)
+		{
+		case IDM_SQUARE_BLUE:	SetSkin(hwnd, "square_blue"); break;
+		case IDM_METAL_MISTRAL:	SetSkin(hwnd, "metal_mistral"); break;
+		case IDM_EXIT:	SendMessage(hwnd, WM_CLOSE, 0, 0); break;
+		}
+		DestroyMenu(cmMain);
+	}
+	break;
 	case WM_DESTROY:
 		FreeConsole();
 		PostQuitMessage(0);
@@ -410,7 +436,7 @@ VOID SetSkin(HWND hwnd, CONST CHAR skin[])
 {
 	CHAR sz_filename[FILENAME_MAX] = {};
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 18; i++)
 	{
 		sprintf(sz_filename, "ButtonsBMP\\%s\\button_%i.bmp", skin, i);
 		HWND hButton = GetDlgItem(hwnd, IDC_BUTTON_0 + i);
@@ -420,21 +446,9 @@ VOID SetSkin(HWND hwnd, CONST CHAR skin[])
 			sz_filename,
 			IMAGE_BITMAP,
 			i == 0 ? g_i_BUTTON_DOUBLE_SIZE : g_i_BUTTON_SIZE,
-			g_i_BUTTON_SIZE,
+			i == 17 ? g_i_BUTTON_DOUBLE_SIZE : g_i_BUTTON_SIZE,
 			LR_LOADFROMFILE
 		);
 		SendMessage(hButton, BM_SETIMAGE, 0, (LPARAM)bmpButton);
 	}
-	/*sprintf(sz_filename, "ButtonsBMP\\%s\\button_aster.bmp", skin, IDC_BUTTON_ASTER);
-	HWND hButton = GetDlgItem(hwnd, IDC_BUTTON_ASTER);
-	HBITMAP bmpButton = (HBITMAP)LoadImage
-	(
-		GetModuleHandle(NULL),
-		sz_filename,
-		IMAGE_BITMAP,
-		g_i_BUTTON_SIZE,
-		g_i_BUTTON_SIZE,
-		LR_LOADFROMFILE
-	);
-	SendMessage(hButton, BM_SETIMAGE, 0, (LPARAM)bmpButton);*/
 }
